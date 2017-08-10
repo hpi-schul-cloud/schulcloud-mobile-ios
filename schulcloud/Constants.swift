30567c069b987eae4104029a07b0a7b6756ea16f
Max Bothe <mathebox@gmail.com>
2017-08-10 16:51:22 +0200

    Fix loading of course content (#36)
    * Fix loading coruse content by using cookie.
    
    * Scale images in course content. Improve placeholder for unsupported content.

--- schulcloud/Constants.swift
+++ schulcloud/Constants.swift
@@ -12,6 +12,7 @@ class Constants {
     enum Servers: String {
         case staging = "https://schul.tech:3030/"
         case production = "https://schul-cloud.org:8080/"
+        case web = "https://schul-cloud.org/"
         
         var url: URL {
             return URL(string: self.rawValue)!
@@ -20,5 +21,14 @@ class Constants {
     
     static let backend = Servers.production
     
-    static let textStyleHtml = "<style>body{font-family: 'PT Sans', '-apple-system'; font-size: 17px;}a {color: #b10438; text-decoration: none}</style>"
+    static var textStyleHtml: String {
+        var style: String = "<style>"
+        style += "body {font-family: 'PT Sans', '-apple-system'; font-size: 17px;}"
+        style += "a {color: #b10438; text-decoration: none}"
+        style += "img {display: block; max-width: 100%; width: auto !important; height: auto !important;}"
+        style += ".not-supported {border: 1px solid #aaa; background-color: #ddd; border-radius: 2px; padding: 8px 4px; display: block; max-width: 100%; width: auto !important; text-align: center}"
+        style += "</style>"
+        return style
+    }
+
 }
