ba088a85e7f737bd3879708be64971ae57a38bbd
Carl Gödecken <carl.goedecken@student.hpi.de>
2017-09-14 17:32:35 +0200

    Changes to how courses and teachers are fetched

--- schulcloud/Course+.swift
+++ schulcloud/Course+.swift
@@ -9,9 +9,14 @@
 import Foundation
 import CoreData
 import Marshal
+import BrightFutures
 
 extension Course {
-    static func upsert(data: MarshaledObject, context: NSManagedObjectContext) throws -> Course {
+    
+    static let fetchQueue = DispatchQueue.init(label: "course", qos: .default, attributes: .concurrent, autoreleaseFrequency: .inherit, target: nil)
+    
+    static func upsert(data: MarshaledObject, context: NSManagedObjectContext) -> Future<Course, SCError> {
+        do {
             let course = try self.findOrCreateWithId(data: data, context: context)
             
             course.name = try data.value(for: "name")
@@ -19,7 +24,34 @@ extension Course {
             course.descriptionText = try data.value(for: "description")
             course.colorString = try data.value(for: "color")
             
-            return course
+            let teacherIds: [String] = (try? data.value(for: "teacherIds")) ?? [String]()
+            return course.fetchTeachers(by: teacherIds, context: context)
+                .recoverWith {
+                    log.error($0)
+                    return Future(value: course)
+            }
+        } catch let error {
+            return Future(error: SCError.jsonDeserialization(error.description))
+        }
+    }
+    
+    fileprivate func fetchTeachers(by ids: [String], context: NSManagedObjectContext) -> Future<Course, SCError> {
+        let teacherFutures = ids.map {
+            User.fetch(by: $0, inContext: context)
+        }
+        return teacherFutures.sequence().flatMap { teachers -> Future<Course, SCError> in
+            self.teachers = NSSet(array: teachers)
+            return Future(value: self)
+        }
+    }
+    
+    static func fetch(by id: String, inContext context: NSManagedObjectContext) -> Future<Course, SCError> {
+        if let _course = try? Course.find(by: id, context: context),
+            let course = _course {
+            return Future<Course, SCError>(value: course)
+        }
+        return ApiHelper.request("courses/\(id)").jsonObjectFuture()
+            .flatMap { Course.upsert(data: $0, context: context) }
     }
 }
 
