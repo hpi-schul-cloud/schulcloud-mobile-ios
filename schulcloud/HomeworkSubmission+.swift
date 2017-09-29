51f85e53ffa1e1d7e2ba6aa56eb394078ec453b1
Carl Gödecken <carl.goedecken@student.hpi.de>
2017-09-29 23:22:16 +0200

    Add homework submission methods

--- schulcloud/HomeworkSubmission+.swift
+++ schulcloud/HomeworkSubmission+.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+import BrightFutures
+import CoreData
+import Marshal
+
+extension HomeworkSubmission {
+    static func upsert(inContext context: NSManagedObjectContext, object: MarshaledObject, for homework: Homework) -> Future<HomeworkSubmission, SCError> {
+        do {
+            let submission = try HomeworkSubmission.findOrCreateWithId(data: object, context: context)
+            
+            submission.homework = homework
+            submission.comment = try object.value(for: "comment")
+            submission.grade = (try? object.value(for: "grade")) ?? 0
+            submission.gradeComment = try? object.value(for: "gradeComment")
+            
+            let studentId: String? = try? object.value(for: "studentId")
+            let studentFuture = submission.fetchUser(by: studentId, context: context).onErrorLogAndRecover(with: Void())
+            
+            return studentFuture.flatMap(object: homework).flatMap(object: submission)
+        } catch let error as MarshalError {
+            return Future(error: .jsonDeserialization(error.description))
+        } catch let error {
+            return Future(error: .database(error.localizedDescription))
+        }
+    }
+    
+    func fetchUser(by id: String?, context: NSManagedObjectContext) -> Future<Void, SCError> {
+        guard let id = id else { return Future(value: Void())}
+        return User.fetchQueue.sync {
+            return User.fetch(by: id, inContext: context)
+                .andOnSuccess { self.student = $0 }
+                .asVoid()
+        }
+    }
+}
+
+extension HomeworkSubmission: IdObject {
+    static let entityName = "HomeworkSubmission"
+}
+
+extension HomeworkSubmission: Marshaling {
+    public func marshaled() -> [String: Any] {
+        return [
+            "_id": self.id,
+            "schoolId": self.student.schoolId,
+            "studentId": self.student.id,
+            "homeworkId": self.homework.id,
+            "comment": self.comment
+        ]
+    }
+}
--- schulcloud/HomeworkSubmissionHelper.swift
+++ schulcloud/HomeworkSubmissionHelper.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import BrightFutures
+import CoreData
+
+extension HomeworkSubmission {
+    
+    func upload() -> Future<Void, SCError> {
+        let body = self.marshaled()
+        return ApiHelper.request("submissions", method: .post, parameters: body).responseJSONFuture()
+            .recoverWith { _ in ApiHelper.request("submissions/\(self.id)", method: .patch, parameters: body).responseJSONFuture() }
+            .asVoid()
+    }
+}
+
+struct HomeworkSubmissionHelper {
+    static func save(for homework: Homework, existing: HomeworkSubmission?, submissionText: String) -> Future<Void, SCError> {
+        let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        context.parent = managedObjectContext
+        let submission = existing ?? HomeworkSubmission(context: managedObjectContext)
+        submission.comment = submissionText
+        submission.homework = homework
+        return User.getCurrent(inContext: context)
+            .andOnSuccess { submission.student = $0 }
+            .flatMap { _ in submission.upload() }
+            .onSuccess {
+                do {
+                    try context.save()
+                    try managedObjectContext.save()
+                } catch let error {
+                    log.error(error.description)
+                }
+            }
+    }
+}
