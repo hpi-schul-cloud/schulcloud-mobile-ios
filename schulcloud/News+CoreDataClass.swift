9891712d1a65a7d7968c45722c2b3b237f75c7c9
Florian MOREL <m.morel.florian@gmail.com>
2018-01-08 10:00:48 +0100

    NewsArticle model (#99)
    * Adding news article model and processing logic

---
+++ schulcloud/News+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  News+CoreDataClass.swift
+//  
+//
+//  Created by Florian Morel on 03.01.18.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(NewsArticle)
+public class NewsArticle: NSManagedObject {
+
+}
---
+++ schulcloud/News+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  News+CoreDataProperties.swift
+//  
+//
+//  Created by Florian Morel on 03.01.18.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension NewsArticle {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<NewsArticle> {
+        return NSFetchRequest<NewsArticle>(entityName: "NewsArticle")
+    }
+
+    @NSManaged public var content: String
+    @NSManaged public var createdAt: NSDate
+    @NSManaged public var displayAt: String
+    @NSManaged public var history: [String]
+    @NSManaged public var id: String
+    @NSManaged public var schoolId: String?
+    @NSManaged public var title: String
+    @NSManaged public var updatedAt: NSDate?
+    @NSManaged public var creator: User?
+}
