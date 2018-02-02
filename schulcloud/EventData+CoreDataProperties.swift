48d3bfda6b6f89cc4a52313a7474c74b7f62f18e
Florian MOREL <m.morel.florian@gmail.com>
2018-02-02 15:28:47 +0100

    Use of local data for calendar events instead of EventKit (#101)
    * Remove required EventKit permission
    * Store events in CoreData
    * Add switch to enable sync to calendar app

--- schulcloud/EventData+CoreDataProperties.swift
+++ schulcloud/EventData+CoreDataProperties.swift
@@ -16,8 +16,19 @@ extension EventData {
         return NSFetchRequest<EventData>(entityName: "EventData")
     }
 
-    @NSManaged public var externalEventId: String
-    @NSManaged public var courseId: String?
-    @NSManaged public var eventId: String
-
+    @NSManaged var id: String
+    @NSManaged var title: String
+    @NSManaged var detail: String
+    @NSManaged var location: String
+    @NSManaged var start: NSDate
+    @NSManaged var end: NSDate
+    
+    @NSManaged var rrFrequency: String?
+    @NSManaged var rrDayOfWeek: String?
+    @NSManaged var rrEndDate: NSDate?
+    @NSManaged var rrInterval: Int32
+    
+    @NSManaged var course: Course?
+    
+    @NSManaged var ekIdentifier: String?
 }
