3e4f4c341a49937c3a4389a89c4131a5f07c3cd8
Max Bothe <max.bothe@hpi.de>
2018-02-22 11:19:01 +0100

    Remove more code

--- schulcloud/BrightFutures+.swift
+++ schulcloud/BrightFutures+.swift
@@ -6,20 +6,21 @@
 //  Copyright © 2017 Hasso-Plattner-Institut. All rights reserved.
 //
 
-import Foundation
-import BrightFutures
+//import Foundation
+//import BrightFutures
+//
+//extension Future {
+//    func flatMap<U>(object: U) -> Future<U, Future.Value.Error> {
+//        return self.flatMap({ _ -> Future<U, Future.Value.Error> in
+//            return Future<U, Future.Value.Error>(value: object)
+//        })
+//    }
+//
+//    func onErrorLogAndRecover(with object: Value.Value) -> Future<Value.Value, Future.Value.Error> {
+//        return self.recoverWith { (error) -> Future<Value.Value, Future.Value.Error> in
+//            log.error(error)
+//            return Future(value: object)
+//        }
+//    }
+//}
 
-extension Future {
-    func flatMap<U>(object: U) -> Future<U, Future.Value.Error> {
-        return self.flatMap({ _ -> Future<U, Future.Value.Error> in
-            return Future<U, Future.Value.Error>(value: object)
-        })
-    }
-    
-    func onErrorLogAndRecover(with object: Value.Value) -> Future<Value.Value, Future.Value.Error> {
-        return self.recoverWith { (error) -> Future<Value.Value, Future.Value.Error> in
-            log.error(error)
-            return Future(value: object)
-        }
-    }
-}
--- schulcloud/Date+Marshal.swift
+++ schulcloud/Date+Marshal.swift
@@ -9,7 +9,8 @@
 import Foundation
 import Marshal
 
-extension Date {
+extension Date : ValueType {
+
     static let iso8601Formatter: ISO8601DateFormatter = {
         let formatter = ISO8601DateFormatter()
         formatter.formatOptions = [.withFullDate,
@@ -18,9 +19,7 @@ extension Date {
                                    .withColonSeparatorInTime]
         return formatter
     }()
-}
 
-extension Date : ValueType {
     public static func value(from object: Any) throws -> Date {
         guard let dateString = object as? String else {
             throw MarshalError.typeMismatch(expected: String.self, actual: type(of: object))
@@ -31,19 +30,20 @@ extension Date : ValueType {
         }
         return date
     }
+
 }
 
-extension NSDate : ValueType {
-    public static func value(from object: Any) throws -> NSDate {
-        guard let dateString = object as? String else {
-            throw MarshalError.typeMismatch(expected: String.self, actual: type(of: object))
-        }
-
-        guard let date = Date.iso8601Formatter.date(from: dateString) as NSDate? else {
-            throw MarshalError.typeMismatch(expected: "ISO8601 date string", actual: dateString)
-        }
-        return date
-    }
-}
+//extension NSDate : ValueType {
+//    public static func value(from object: Any) throws -> NSDate {
+//        guard let dateString = object as? String else {
+//            throw MarshalError.typeMismatch(expected: String.self, actual: type(of: object))
+//        }
+//
+//        guard let date = Date.iso8601Formatter.date(from: dateString) as NSDate? else {
+//            throw MarshalError.typeMismatch(expected: "ISO8601 date string", actual: dateString)
+//        }
+//        return date
+//    }
+//}
 
 
