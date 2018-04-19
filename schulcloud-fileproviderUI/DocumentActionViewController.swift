2a7f13cf9520ec6e2b7176074b6e81abc270ba5c
Florian MOREL <m.morel.florian@gmail.com>
2018-04-19 11:18:33 +0200

    Fileprovider + UI added

---
+++ schulcloud-fileproviderUI/DocumentActionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  Created for schulcloud-mobile-ios under GPL-3.0 license.
+//  Copyright © HPI. All rights reserved.
+//
+
+import UIKit
+
+class DocumentActionViewController: UIDocumentPickerExtensionViewController {
+
+    override func prepareForPresentation(in mode: UIDocumentPickerMode) {
+
+    }
+}
+
