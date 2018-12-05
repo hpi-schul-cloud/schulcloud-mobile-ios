0a5927854a51a18a47cdfd7682f9540f26524375
Max Bothe <max.bothe@hpi.de>
2018-12-05 17:32:32 +0100

    Refactor CourseCell

--- iOS/Views/CourseCell.swift
+++ iOS/Views/CourseCell.swift
@@ -12,23 +12,26 @@ class CourseCell: UICollectionViewCell {
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var teacherLabel: UILabel!
 
-    func configure(for course: Course) {
+    override func awakeFromNib() {
         self.layer.masksToBounds = true
-        self.layer.cornerRadius = 4.0
+        self.layer.cornerRadius = 6.0
         self.layer.borderWidth = 0.5
         self.layer.borderColor = UIColor(white: 0.7, alpha: 1.0).cgColor
+    }
 
+    func configure(for course: Course) {
         self.titleLabel.text = course.name
+
         if course.teachers.isEmpty {
             self.teacherLabel.isHidden = true
         } else {
-            let namesAbbreviated = course.teachers.map { $0.shortName }
-            self.teacherLabel.text = namesAbbreviated.joined(separator: ", ")
+            let names = course.teachers.map { $0.shortName }
+            self.teacherLabel.text = names.joined(separator: ", ")
             self.teacherLabel.isHidden = false
         }
 
-        if let color = course.colorString {
-            self.colorView.backgroundColor = UIColor(hexString: color)
-        }
+        let courseColor = course.colorString.flatMap { UIColor(hexString: $0) }
+        self.colorView.backgroundColor = courseColor ?? .white
     }
+
 }
