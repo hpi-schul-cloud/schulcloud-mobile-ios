5552d131440ed65bc669e64f91a4c294344f986a
Florian MOREL <m.morel.florian@gmail.com>
2019-01-09 12:48:08 +0100

    Show Image picker and save image for upload

--- iOS/Controllers/HomeworkList/HomeworkDetailViewController.swift
+++ iOS/Controllers/HomeworkList/HomeworkDetailViewController.swift
@@ -13,6 +13,7 @@ class HomeworkDetailViewController: UIViewController {
     @IBOutlet private weak var contentLabel: UILabel!
     @IBOutlet private weak var coloredStrip: UIView!
     @IBOutlet private weak var dueLabel: UILabel!
+    @IBOutlet weak var submitHomeworkButton: UIButton!
 
     var homework: Homework?
 
@@ -31,5 +32,55 @@ class HomeworkDetailViewController: UIViewController {
         self.coloredStrip.backgroundColor = homework.color
 
         self.contentLabel.attributedText = HTMLHelper.default.attributedString(for: homework.descriptionText)
+    }
+    @IBAction func submitHomework(_ sender: Any) {
+        let picker = UIImagePickerController()
+
+        let actionController = UIAlertController()
+        let cameraAction = UIAlertAction(title: "Taking a picture", style: .default) { [unowned self] action in
+
+            picker.sourceType = .camera
+            picker.allowsEditing = true
+            picker.cameraCaptureMode = .photo
+            picker.cameraDevice = .rear
+            picker.showsCameraControls = true
+            self.present(picker, animated: true)
+        }
+
+        let libraryAction = UIAlertAction(title: "Photo Library", style: .default) { [unowned self] action in
+            let picker = UIImagePickerController()
+            picker.sourceType = .savedPhotosAlbum
+            picker.allowsEditing = false
+
+            self.present(picker, animated: true)
+        }
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+
+        [cameraAction, libraryAction, cancelAction].forEach { actionController.addAction($0) }
+        self.present(actionController, animated: true)
+    }
+}
+
+extension HomeworkDetailViewController: UIImagePickerControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // TODO: Save locally, prepare for upload
+        guard let image = info[.editedImage] as? UIImage else {
+            return
+        }
+        do {
+            // TOoDO: find better submission file naem
+            let destURL = try FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent("picture_submission_\(self.homework?.id ?? "")")
+            let jpegData = image.jpegData(compressionQuality: 0.7)
+            try jpegData?.write(to: destURL, options: .withoutOverwriting)
+        } catch _ {
+
+        }
+
+        // Schedule upload of image
     }
 }
