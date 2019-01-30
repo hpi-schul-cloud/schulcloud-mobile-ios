d5a54bb3f479c333b71dddcd987c017814e64df9
Florian MOREL <m.morel.florian@gmail.com>
2019-01-30 10:53:33 +0100

    Base functional submission form

--- iOS/Controllers/HomeworkList/HomeworkSubmitViewController.swift
+++ iOS/Controllers/HomeworkList/HomeworkSubmitViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 fileprivate let addFileFooterIdentifier = "addFileFooter"
 
+final class HomeworkSubmitStudentCommentCell: UITableViewCell {
+    @IBOutlet var commentField: UITextView!
+}
+
+final class HomeworkSubmitTableFooterView: UIView {
+
+}
+
 final class HomeworkSubmitViewController: UITableViewController {
 
     private enum Section: Int, CaseIterable {
@@ -19,15 +27,33 @@ final class HomeworkSubmitViewController: UITableViewController {
     }
 
     var submission: Submission?
-    let fileSync = FileSync.default
+    fileprivate let fileSync = FileSync.default
+    fileprivate let writtingContext = CoreDataHelper.persistentContainer.newBackgroundContext()
+
+    @IBAction func applyChanges(_ sender: Any) {
+        switch self.writtingContext.saveWithResult() {
+        case .failure(let error):
+            //TODO Display error
+            print("Error saving changes: \(error)")
+        case .success(_):
+            SubmissionHelper.saveSubmission(item: self.submission!)
+        }
+    }
+
+    @IBAction func discardChanges(_ sender: Any) {
+        self.writtingContext.rollback()
+    }
 
     override func viewDidLoad() {
         let nib = UINib(nibName: "HomeworkSubmitFileButton", bundle: nil)
         self.tableView.register(nib, forHeaderFooterViewReuseIdentifier: addFileFooterIdentifier)
+        self.tableView.keyboardDismissMode = .onDrag
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return Section.allCases.count
+        var count = 2
+        if self.submission?.gradeComment?.isEmpty == false { count += 1 }
+        return count
     }
 
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -54,30 +80,42 @@ final class HomeworkSubmitViewController: UITableViewController {
         }
     }
 
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        guard let section = Section(rawValue: section), section == .files else { return 0.0 }
+        return 44
+    }
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let section = Section(rawValue: indexPath.section) else { fatalError() }
 
-        var maybeCell = tableView.dequeueReusableCell(withIdentifier: "homeworkCell")
-        if maybeCell == nil {
-            maybeCell = UITableViewCell(style: .default, reuseIdentifier: "homeworkCell")
-        }
-
-        let cell = maybeCell!
-
-        let files = Array(self.submission?.files ?? [])
-
-        var text: String? = nil
         switch section {
         case .comment:
-            text = self.submission?.comment
-        case .files:
-            text = files[indexPath.row].name
-        case .teacherComment:
-            cell.textLabel?.text = self.submission?.gradeComment
-        }
+            let cell = tableView.dequeueReusableCell(withIdentifier: "commentCell")! as! HomeworkSubmitStudentCommentCell
+            if cell.commentField.text.isEmpty { cell.commentField.text = self.submission?.comment ?? "" }
+            cell.commentField.delegate = self
+            return cell
+        default:
+            var maybeCell = tableView.dequeueReusableCell(withIdentifier: "homeworkCell")
+            if maybeCell == nil {
+                maybeCell = UITableViewCell(style: .default, reuseIdentifier: "homeworkCell")
+            }
+            let cell = maybeCell!
 
-        cell.textLabel?.text = text
-        return cell
+            let files = Array(self.submission?.files ?? [])
+
+            var text: String? = nil
+            switch section {
+            case .comment:
+                text = self.submission?.comment
+            case .files:
+                text = files[indexPath.row].name
+            case .teacherComment:
+                cell.textLabel?.text = self.submission?.gradeComment
+            }
+
+            cell.textLabel?.text = text
+            return cell
+        }
     }
 
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -134,6 +172,11 @@ final class HomeworkSubmitViewController: UITableViewController {
         [cameraAction, libraryAction, cancelAction].forEach { actionController.addAction($0) }
         self.present(actionController, animated: true)
     }
+}
+
+// MARK: - Submission writting
+extension HomeworkSubmitViewController {
+
 }
 
 extension HomeworkSubmitViewController: UINavigationControllerDelegate {
@@ -193,13 +236,10 @@ extension HomeworkSubmitViewController: UIImagePickerControllerDelegate {
     func link(file: File, to submission: Submission, completionHandler: @escaping (Result<Void, SCError>) -> Void ) {
         let fileObjectId = file.objectID
         let submissionObjectID = submission.objectID
-        let context = CoreDataHelper.persistentContainer.newBackgroundContext()
-        context.performAndWait {
-            let file = context.typedObject(with: fileObjectId) as File
-            let submission = context.typedObject(with: submissionObjectID) as Submission
+        self.writtingContext.performAndWait {
+            let file = self.writtingContext.typedObject(with: fileObjectId) as File
+            let submission = self.writtingContext.typedObject(with: submissionObjectID) as Submission
             submission.files.insert(file)
-            context.saveWithResult()
-            SubmissionHelper.saveSubmission(item: submission).onComplete(callback: completionHandler)
         }
     }
 
@@ -215,33 +255,36 @@ extension HomeworkSubmitViewController: UIImagePickerControllerDelegate {
 
         let (task, future) = self.signedURL(resourceAt: remoteURL, mimeType: type, forUpload: true)
         future.flatMap { signedURL -> Future<Void, SCError> in
+            // Upload file
             flatname = signedURL.header[.flatName]!
             thumbnailURL = signedURL.header[.thumbnail]!
 
             let (task, future) = self.upload(fileAt: url, to: signedURL.url, mimeType: type)
             task.resume()
             return future
-            }.flatMap { _ -> Future<[String: Any], SCError> in
-                let (task, future) = self.createFileMetadata(at: remoteURL, mimeType: type, size: size, flatName: flatname, thumbnailURL: URL(string: thumbnailURL)!)
-                task?.resume()
-                return future
-            }.flatMap { json -> Result<File, SCError> in
-                let context = CoreDataHelper.persistentContainer.newBackgroundContext()
-                return context.performAndWait { () -> Result<File, SCError> in
-                    guard let userDirectory = File.by(id: FileHelper.userDirectoryID, in: context) else {
-                        return .failure(SCError.coreDataMoreThanOneObjectFound)
-                    }
-                    do {
-                        let file = try File.createOrUpdate(inContext: context, parentFolder: userDirectory, isDirectory: false, data: json)
-                        context.saveWithResult()
-                        return .success(file)
-                    } catch let error as SCError {
-                        return .failure(error)
-                    } catch let error {
-                        return .failure(SCError.other(error.localizedDescription))
-                    }
+        }.flatMap { _ -> Future<[String: Any], SCError> in
+            // Remotely create the file metadtas
+            let (task, future) = self.createFileMetadata(at: remoteURL, mimeType: type, size: size, flatName: flatname, thumbnailURL: URL(string: thumbnailURL)!)
+            task?.resume()
+            return future
+        }.flatMap { json -> Result<File, SCError> in
+            // Create the local file metadata
+            let context = CoreDataHelper.persistentContainer.newBackgroundContext()
+            return context.performAndWait { () -> Result<File, SCError> in
+                guard let userDirectory = File.by(id: FileHelper.userDirectoryID, in: context) else {
+                    return .failure(SCError.coreDataMoreThanOneObjectFound)
                 }
-            }.onComplete(callback: completionHandler)
+                do {
+                    let file = try File.createOrUpdate(inContext: context, parentFolder: userDirectory, isDirectory: false, data: json)
+                    context.saveWithResult()
+                    return .success(file)
+                } catch let error as SCError {
+                    return .failure(error)
+                } catch let error {
+                    return .failure(SCError.other(error.localizedDescription))
+                }
+            }
+        }.onComplete(callback: completionHandler)
         task?.resume()
     }
 
@@ -264,5 +307,28 @@ extension HomeworkSubmitViewController: UIImagePickerControllerDelegate {
         let promise = Promise<[String: Any], SCError>()
         let task = self.fileSync.createFileMetadata(at: at, mimeType: mimeType, size: size, flatName: flatName, thumbnailURL: thumbnailURL) { promise.complete($0) }
         return (task, promise.future)
+    }
+}
+
+extension HomeworkSubmitViewController: UITextViewDelegate {
+
+    func textViewDidEndEditing(_ textView: UITextView) {
+        guard let submission = self.submission else { return }
+
+        func write(content: String) {
+            let subObjectID = submission.objectID
+            self.writtingContext.performAndWait { [unowned self] in
+                let sub = self.writtingContext.typedObject(with: subObjectID) as Submission
+                sub.comment = content
+            }
+        }
+
+        let content = textView.text ?? ""
+
+        if let comment = submission.comment {
+            if comment != content { write(content: content) }
+        } else {
+            write(content: content)
+        }
     }
 }
