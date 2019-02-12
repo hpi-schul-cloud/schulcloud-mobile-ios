b5ff79b5fb6af18328a511444a133ed45b357600
Florian MOREL <m.morel.florian@gmail.com>
2019-02-12 17:22:42 +0100

    Create submission upon pressing button

--- iOS/Controllers/HomeworkList/HomeworkDetailViewController.swift
+++ iOS/Controllers/HomeworkList/HomeworkDetailViewController.swift
@@ -27,35 +27,48 @@ class HomeworkDetailViewController: UIViewController {
         self.coloredStrip.layer.cornerRadius = self.coloredStrip.frame.size.height / 2.0
         guard let homework = self.homework else { return }
 
-        if homework.submission == nil {
-            let json: [String: Any] = [
-                "schoolId": Globals.currentUser!.schoolId,
-                "studentId": Globals.currentUser!.id,
-                "homeworkId": homework.id,
-                "teamMembers": [Globals.currentUser!.id] as Any,
+        self.configure(for: homework)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.configure(for: self.homework!)
+    }
+
+    @IBAction func submissionPressed(_ sender: UIButton!) {
+
+        guard self.homework!.submission == nil else {
+            self.performSegue(withIdentifier: "showSubmission", sender: nil)
+            return
+        }
+
+        let json: [String: Any] = [
+            "schoolId": Globals.currentUser!.schoolId,
+            "studentId": Globals.currentUser!.id,
+            "homeworkId": self.homework!.id,
+            "teamMembers": [Globals.currentUser!.id] as Any,
             ]
 
-            SubmissionHelper.syncSubmission(studentId: Globals.currentUser!.id, homeworkId: homework.id).flatMap { submissions -> Future<Submission, SCError> in
-                if submissions.objectIds.isEmpty {
-                    return SubmissionHelper.createSubmission(json: json).map({ result -> Submission in
-                        return CoreDataHelper.viewContext.typedObject(with: result.objectId) as Submission
-                    })
-                }
-
-                if submissions.objectIds.count > 1 {
-                    return Future(error: SCError.coreDataMoreThanOneObjectFound)
-                }
-
-                return Future(value: CoreDataHelper.viewContext.typedObject(with: submissions.objectIds.first!))
-            }.onSuccess { submission in
-                DispatchQueue.main.async {
-                    self.configure(for: self.homework!)
-                }
-            }.onFailure { error in
-                print("Failed to sync or create submission, error: %@", error)
+        SubmissionHelper.syncSubmission(studentId: Globals.currentUser!.id,
+                                        homeworkId: homework!.id).flatMap { submissions -> Future<Submission, SCError> in
+            if submissions.objectIds.isEmpty {
+                return SubmissionHelper.createSubmission(json: json).map({ result -> Submission in
+                    return CoreDataHelper.viewContext.typedObject(with: result.objectId) as Submission
+                })
             }
+
+            if submissions.objectIds.count > 1 {
+                return Future(error: SCError.coreDataMoreThanOneObjectFound)
+            }
+
+            return Future(value: CoreDataHelper.viewContext.typedObject(with: submissions.objectIds.first!))
+        }.onSuccess(DispatchQueue.main.context) { submission in
+            self.performSegue(withIdentifier: "showSubmission", sender: nil)
+        }.onFailure(DispatchQueue.main.context) { error in
+            let alertController = UIAlertController(title: "Error", message: "Failed to create submission:\(error.localizedDescription)", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: .cancel))
+            self.present(alertController, animated: true)
         }
-        self.configure(for: homework)
     }
 
     func configure(for homework: Homework) {
@@ -64,8 +77,8 @@ class HomeworkDetailViewController: UIViewController {
         self.dueLabel.text = Homework.dateTimeFormatter.string(from: homework.dueDate)
         self.coloredStrip.backgroundColor = homework.color
 
-        let title = homework.submission != nil ? "Submit file(s) (\(homework.submission!.files.count) submitted)" : "Submit file(s)"
-        self.submitHomeworkButton.titleLabel?.text = title
+        let title = homework.submission != nil ? "Update submission" : "Create a Submission"
+        [UIControl.State.normal, .highlighted, .focused, .disabled].forEach { self.submitHomeworkButton.setTitle(title, for: $0) }
 
         self.contentLabel.attributedText = HTMLHelper.default.attributedString(for: homework.descriptionText)
     }
